42ad0a75119dc1e2877ddf8b5292dc7f46bc847a
michal-at-git <phantom-black@myopera.com>
2012-12-28 18:52:54 +0100

    js-calc is finished but visual style... not! :)

--- js-calc/js-calc.js
+++ js-calc/js-calc.js
@@ -1,20 +1,27 @@
 function test(txt) {
+
  var lcd = document.getElementById('display');
   var numbs = lcd.value; 
-
- if (txt >= 0 && txt <=9 && txt != '+' && txt != '-' && txt != '*' && txt != '/')
+  
+ if (txt != '+' && txt != '-' && txt != '*'  && txt != '/' && txt != '=')
  {
   document.getElementById('display').innerHTML += txt;
  }
- else
+ 
+ 
+ 
+ else 
  {	
+   
+	   
    if (numbs.slice(-1) == '+' || numbs.slice(-1) == '*' || numbs.slice(-1) == '-' || numbs.slice(-1) == '/')
 	  { 
 	     backup = numbs.slice(numbs.lenght,-1);
 	    
 	   document.getElementById('display').innerHTML = backup;
 	   document.getElementById('display').innerHTML += txt;
-
+	   
+	  
 	  }
 	  else 
 	  {
@@ -23,7 +30,14 @@ function test(txt) {
 	  }
 
  }
- 	 
+ 	
+if (txt == "=")
+  
+{
+ var result =  eval(numbs);
+ document.getElementById('display').innerHTML = result;
+}
 
+ 
    
 }
