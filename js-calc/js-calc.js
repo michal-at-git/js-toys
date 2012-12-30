0a47e58e634f2f30c0bb759bccb796959524e293
michal-at-git <phantom-black@myopera.com>
2012-12-30 16:05:05 +0100

    js-calc is finished... :)

--- js-calc/js-calc.js
+++ js-calc/js-calc.js
@@ -3,32 +3,28 @@ function jscalc(txt) {
  var lcd = document.getElementById('display');
   var numbs = lcd.value; 
   
- if (txt != '+' && txt != '-' && txt != '*'  && txt != '/' && txt != '=')
+ if (txt != '+' && txt != '-' && txt != '*'  && txt != '/' && txt != '=' && txt != '.')
  {
   document.getElementById('display').innerHTML += txt;
  }
- 
- 
  
  else 
  {	
    
 	   
-   if (numbs.slice(-1) == '+' || numbs.slice(-1) == '*' || numbs.slice(-1) == '-' || numbs.slice(-1) == '/')
+   if (numbs.slice(-1) == '+' || numbs.slice(-1) == '*' || numbs.slice(-1) == '-' || numbs.slice(-1) == '/' || numbs.slice(-1) == '.' )
 	  { 
+	    
 	     backup = numbs.slice(numbs.lenght,-1);
 	    
 	   document.getElementById('display').innerHTML = backup;
 	   document.getElementById('display').innerHTML += txt;
 	   
-	  
 	  }
 	  else 
 	  {
 	    	    document.getElementById('display').innerHTML += txt;
-
 	  }
-
  }
  	
 if (txt == "=")
@@ -37,7 +33,16 @@ if (txt == "=")
  var result =  eval(numbs);
  document.getElementById('display').innerHTML = result;
 }
-
- 
+else if (txt == "c")
+{
+ document.getElementById('display').innerHTML = ''; 
+}
+ else if (txt == "p")
+ {
    
+   var num = parseInt(numbs);
+   num = Math.sqrt(num);
+   document.getElementById('display').innerHTML = num;
+ }
+ 
 }
