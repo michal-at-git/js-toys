67c3408c75b7d2079db47e0d5d7d2ef98bf81de4
michal-at-git <phantom-black@myopera.com>
2013-01-22 23:26:37 +0100

    tic tac toe game: Wanna play? ;> It works\!

--- tic-tac-toe-game/ttt.js
+++ tic-tac-toe-game/ttt.js
@@ -1,14 +1,111 @@
- var i = 0;
-
+ var run = 0;
+var a, b, c, d, e, f, g, h, i;
 function move(mv){
-  if (i == 0)
-  {
+  if (run == 0)
+  { 
+    result(mv, "X");
   document.getElementById(mv).innerHTML= "X";
-  i=1;
+  
+  run=1;
+  resultmap("X");
   }
-   else if (i == 1)
+   else if (run == 1)
   {
+   result(mv, "O");
   document.getElementById(mv).innerHTML= "O";
-  i=0;
+  run =0;
+  resultmap("O");
   }
+}
+
+function result(mv, value) {
+ if (mv == "a") {
+  a=value;
+   
+  }
+  else if (mv == "a") {
+  a=value;
+   
+  }
+  else if (mv == "b") {
+  b=value;
+   
+  }
+   else if (mv == "c") {
+  c=value;
+   
+  }
+  else if (mv == "d") {
+  d=value;
+   
+  }
+   else if (mv == "e") {
+  e=value;
+   
+  }
+  else if (mv == "f") {
+  f=value;
+   
+  }
+   else if (mv == "g") {
+  g=value;
+   
+  }
+   else if (mv == "h") {
+  h=value;
+   
+  }
+  else if (mv == "i") {
+  i=value;
+   
+  }
+}
+
+function resultmap(player) {
+   if (a == player && b == player && c == player)
+    {
+      alert(player+" wins!"); 
+            run = 2;
+
+    }
+    else if (a == player && d == player && g == player)
+       {
+      alert(player+" wins!") ; 
+          run = 2;
+
+    }
+     else if (a == player && e == player && i == player)
+       {
+      alert(player+" wins!"); 
+           run = 2;
+
+    }
+    else if (d == player && e == player && f == player)
+       {
+      alert(player+" wins!"); 
+          run = 2;
+    }
+     else if (g == player && h == player && i == player)
+       {
+      alert(player+" wins!"); 
+           run = 2;
+
+    }
+     else if (b == player && e == player && h == player)
+       {
+      alert(player+" wins!"); 
+           run = 2;
+
+    }
+     else if (c == player && f == player && i == player)
+       {
+      alert(player+" wins!"); 
+           run = 2;
+
+    }
+    else if (c == player && e == player && g == player)
+       {
+      alert(player+" wins!"); 
+      run = 2;
+    }
 }
