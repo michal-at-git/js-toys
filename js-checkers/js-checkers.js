09714ae7cadd0b9163150c8bde1b0d69671e9203
michal-at-git <phantom-black@myopera.com>
2013-01-26 20:24:14 +0100

    js-checkers: Now pawns are real...:)

--- js-checkers/js-checkers.js
+++ js-checkers/js-checkers.js
@@ -1,4 +1,28 @@
-function checkers(point)
+function start() {
+  var white = ['aa', 'ca', 'ea', 'ga', 'bb', 'db', 'fb', 'hb'];
+  var black = ['bh', 'dh', 'fh', 'hh','ag', 'cg', 'eg', 'gg'];
+  var position = [ null, null, null, null, null, null, null, null, 
+  null, null, null, null, null, null, null, 
+  null, null, null, null, null, null, null,
+  null, null, null, null, null, null, null, null, null, null];
+
+  var i = 0;
+  while (i < 8)
+  { position[i] = "white";
+    position[32-i] = "black"; 
+
+ document.getElementById(white[i]).innerHTML = "&#x25cf;";
+ document.getElementById(black[i]).innerHTML = "&#x25cb;";
+  
+ ++i;
+  }
+}
+
+function checkers(position)
 {
- alert(point); 
+ return 0;
+}
+
+function move (from, cell) {
+  return 0;
 }
