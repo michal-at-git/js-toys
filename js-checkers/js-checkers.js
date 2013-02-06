4f7b1c1dfa6fe369856f5c9eeb31e12f016c7cae
michal-at-git <phantom-black@myopera.com>
2013-02-06 07:19:00 +0100

    [js-checkers]: highlight

--- js-checkers/js-checkers.js
+++ js-checkers/js-checkers.js
@@ -12,17 +12,13 @@ var position = [ /*aa*/null, /*ca*/ null, /*ea*/ null,/*ga*/ null, /*bb*/null, /
 	      'bh', 'dh', 'fh', 'hh',
 	     ]
 function start() {
-  var white = ['aa', 'ca', 'ea', 'ga', 'bb', 'db', 'fb', 'hb'];
-  var black = ['bh', 'dh', 'fh', 'hh','ag', 'cg', 'eg', 'gg'];
-  
-
-  var i = 0;
+   var i = 0;
   while (i < 8)
   { position[i] = "white";
     position[31-i] = "black"; 
 
- document.getElementById(white[i]).innerHTML = "&#x25cf;";
- document.getElementById(black[i]).innerHTML = "&#x25cb;";
+ document.getElementById(board[i]).innerHTML = "&#x25cf;";
+ document.getElementById(board[31-i]).innerHTML = "&#x25cb;";
   
  ++i;
   }
@@ -39,11 +35,15 @@ function checkers(inpt)
    while (board[i] != inpt) {
      ++i;
    }
-   if (position[i]) move = 1;
-   else {
-     move = 0
-    i = 0, j = 0;
+   if (position[i]){
+     move = 1;
+     hilight(board[i], move);
     }
+   else {
+     move = 0;     
+      i = 0, j = 0;
+    }
+
  }
  
  else { 
@@ -56,13 +56,14 @@ function checkers(inpt)
    position[j] = position[i];
    position[i] = null;
    refresh();
+   move = 0;
+   hilight(board[i], move);
   }
- 
-  move = 0;
+  
+
   i = 0, j = 0;
    
  }
- //alert(move);
  return 0;
 }
 
@@ -80,6 +81,26 @@ function refresh() {
   }
 }
 
+function hilight(board, move)
+ {
+ if (move == 1)
+ {
+    var light = document.getElementById(board);
+    with (light.style) 
+    {
+	borderColor = 'red';
+    }
+ }
+    else{ 
+	var light = document.getElementById(board);
+	with (light.style) 
+	    {
+	      borderColor = '#000000';
+	    }
+	}
+ }
+
+
 function rules(i, j, pawn) {
   if (pawn == "white")
   {
@@ -94,17 +115,3 @@ function rules(i, j, pawn) {
  else return true; 
   }
 }
-
-
-/*
-function move (from, cell) {
-  if (from 
-  return 0;
-}
-
-function cell(pos) {
-  if(pos='aa') return 0; if (pos == 'aa') return 1; if (pos == 'ca') return 2 if (pos == 'ea') return 3;
-      if (pos == 'ga') return 4; if (pos == 'bb') return 5; if(pos == 'db') return 6, if (pos == 'fb') return 7; if (pos == 'hb') return 8; if (pos =='ac') return 9;
-      if (pos == 'cc') return 10; if (pos == 'ec') return 11; if (pos == 'gc') return 12; if (pos == 'bd') return 13; if (pos == dd) return 14;
-      if pos == 'fd') return 15; if pos == 'hd') return 16;  
-}*/
