b3fc980d96e5133417b4597bd923374fdb25bc86
michal-at-git <phantom-black@myopera.com>
2013-02-08 16:21:32 +0100

    [js-checkers]: Moves are working on mv()

--- js-checkers/js-checkers.js
+++ js-checkers/js-checkers.js
@@ -28,6 +28,12 @@ var _in = 0;
 var _out = 0;
 var i = 0, j = 0;
 
+
+
+
+
+
+////////////////////// <checkers()>
 function checkers(inpt)
 { 
  
@@ -51,9 +57,9 @@ function checkers(inpt)
      ++j;
    }
    
-  if (rules(i,j, position[i]) && mv(i, j))//test imp of mv()!!!
- 
+  if ((mv(i, j, position[i])))//test imp of mv()!!!
   {
+   alert("true");
    position[j] = position[i];
    position[i] = null;
    move = 0;
@@ -69,6 +75,88 @@ function checkers(inpt)
  }
  return 0;
 }
+
+
+///////////////////// </checkers()>
+
+
+
+//will be renamed to move()! conflict with wariable name ;P
+function mv(from, to, pawn) {
+ //lines
+
+ from += 1;
+ to += 1;
+ var res = false;
+ var line = []
+ line[0] = [1,2,3,4];
+ line[1] = [5,6,7,8];
+ line[2] = [9,10,11,12];
+ line[3] = [13,14,15,16];
+ line[4] = [17,18,19,20];
+ line[5] = [21,22,23,24];
+ line[6] = [25,26,27,28];
+ line[7] = [29,30,31,32];
+
+if (pawn == "white") 
+ for (x = 0; x <= 7; x++)
+ {
+   for (y = 0; y <= 6; y++)
+   { 
+      if ((from == line[x][y]) && (to == line[x+1][y]))  
+      {
+	alert("legal move");
+
+	x = 7;
+	res = true;
+      }
+      else if ((from == line[x][y+1]) && (to == line[x+1][y])) // not finished & not good
+	{
+	  alert("legal move");
+	  x = 7;
+	  res = true;
+	}
+
+   }
+ }
+ 
+else if (pawn == "black")
+{
+  for (x = 7; x >= 0; x--)
+ {
+  
+   for (y = 6; y >= 1; y--)
+   { 
+      if ((from == line[x][y]) && (to == line[x-1][y]))  
+      {
+	alert("legal move");
+
+	x = 0;
+	res = true;
+      }
+      else if ((from == line[x][y-1]) && (to == line[x-1][y])) // UNSTABLE - NOT WORKING
+	{
+	  alert("legal move");
+	  x = 0;
+	  res = true;
+	}
+
+   }
+ }
+}
+
+ 
+ return res;
+}
+
+
+
+
+
+
+
+
+
 
 function refresh() {
   var i;
@@ -104,57 +192,17 @@ function highlight(board, move)
  }
 
 
-function rules(i, j, pawn) {
-  if (pawn == "white")
-  {
- if (i>j) return false;
- else if (i == j) return false;
- else return true; 
-  }
-  else if (pawn == "black")
-  {
-    if (i<j) return false;
- else if (i == j) return false;
- else return true; 
-  }
-}
-
-//will be renamed to move()! conflict with wariable name ;P
-function mv(from, to) {
- //lines
- from += 1;
- to += 1;
- var res = 0;
- var line = []
- line[0] = [1,2,3,4];
- line[1] = [5,6,7,8];
- line[2] = [9,10,11,12];
- line[3] = [13,14,15,16];
- line[4] = [17,18,19,20];
- line[5] = [21,22,23,24];
- line[6] = [25,26,27,28];
- line[7] = [29,30,31,32];
-
- for (x = 0; x <= 7; x++)
- {
-   for (y = 0; y <= 6; y++)
-   { 
-      if ((from == line[x][y]) && (to == line[x+1][y]))  
-      {
-	alert("legal move");
-
-	x = 7;
-	res = 1;
-      }
-      else if ((from == line[x][y+1]) && (to == line[x+1][y])) // not finished & not good
-	{
-	  alert("legal move");
-	  x = 7;
-	  res = 1;
-	}
-
-   }
- }
-
- return res;
-}
+// function rules(i, j, pawn) {
+//   if (pawn == "white")
+//   {
+//  if (i>j) return false;
+//  else if (i == j) return false;
+//  else return true; 
+//   }
+//   else if (pawn == "black")
+//   {
+//     if (i<j) return false;
+//  else if (i == j) return false;
+//  else return true; 
+//   }
+// }
