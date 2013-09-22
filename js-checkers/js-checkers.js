8f61142903640832668026b943ef834cc88c25d2
michal-at-git <phantom-black@myopera.com>
2013-09-22 20:19:32 +0200

    [js-checkers] simplified checkers function & little updates

--- js-checkers/js-checkers.js
+++ js-checkers/js-checkers.js
@@ -24,8 +24,8 @@ function start() {
       { 
 	document.getElementById(board[y][x]).innerHTML = "&#x25cf;"; //white
  	document.getElementById(board[7-y][x]).innerHTML = "&#x25cb;"; //black
-	position[y][x] = "&#x25cf;"; //white
-	position[7-y][x] = "&#x25cb;"; //black
+	position[y][x] = "white"; //white
+	position[7-y][x] = "black"; //black
 	++x;      
       }
       x = 0;
@@ -33,7 +33,8 @@ function start() {
   }
 }
 var whitemv = true; // white first
-var move = {"from":false, "to":false}
+var from = {"x":false, "y":false}; 
+var to = {"x":false, "y":false};
 // var _in = 0;
 // var _out = 0;
 // var i = 0, j = 0;
@@ -47,69 +48,46 @@ var move = {"from":false, "to":false}
 function checkers(py, px)
 { alert(px+", "+py)
 
-  if(whitemv) {
-    alert("legal move for white"); //debug
-   whitemv=false;
-
-  }
-  else {
+  if(whitemv && position[py][px] && !from["x"] && !from["y"]) {
     
-   alert("legal move for black"); //debug
-   whitemv=true;
-  
-  }
+	alert("legal move for white"); //debug
+	alert("from"); //debug
+
+	from["x"] = px;
+	from["y"] = py;
+    }
+    
+    else if (from["x"] && from["y"])
+    {
+      to["x"] = px;
+      to["y"] = py;
+      if(whitemv) {
+	mv("white");
+	whitemv = false;
+      }
+      else {
+	mv("black"); 
+	whitemb = true;
+      }
+    }
+//     else {    alert("to"); //debug
+// 
+//       to["x"] = px;
+//       to["y"] = py;
+//       mv("white");
+//     }
+//    whitemv=false;
+
+//   }
+//   else if (from["x"] && from["y"]) {
+//     
+//    alert("legal move for black"); //debug
+//    whitemv=true;
+//   
+//   }
   
   highlight(px,py);
- /*
- if (move == 0) {
-   alert("move == 0"); /////////////////////////
-   while (board[i] != inpt) {
-     ++i;
-   }
-   if (position[i]){
-     move = 1;
-   highlight(board[i], move);
-    }
-   else {
-     alert("bad move!");
-     move = 0;   
-     i = 0;
-    }
-
- }
  
- else 
- {
-    while (board[j] != inpt)
-    {
-      ++j;
-    }
-   
-    if (mv(i, j, position[i]))//test imp of mv()!!!
-    {  
-      alert("move == 1:   if (mv(i, j, position[i]))"); /////////////////////////
-
-      position[j] = position[i];
-      position[i] = null;
-      highlight(board[i], 0);
-      refresh();
-      i = 0, j = 0;
-      move = 0;
-
-  }
-   else
-   {
-           alert("move == 1:   else move = 1"); /////////////////////////
-	  move = 0; 
-	  highlight(board[i], move);
-	  j = 0;
-	  i = 0;
-   }
- }
-
-   
- 
- return 0;*/
 }
 
 
@@ -118,9 +96,14 @@ function checkers(py, px)
 
 
 //will be renamed to move()! conflict with wariable name ;P
-function mv(from, to, pawn) {
+function mv(pawn) {
  //lines
-
+ 
+ 
+//finnaly: 
+from["x"] = from["y"] = to["x"] = to["y"] = false; 
+ 
+/*
  from += 1;
  to += 1;
  var res = false;
@@ -195,6 +178,7 @@ else if (pawn == "black")
 
  
  return res;
+ 8*/
 }
 
 
@@ -246,75 +230,14 @@ function highlight(px, py)
  }
 
  
-function isbusy(arg)
-{
- if (arg != null)
- {
-   alert("busy: "+arg); 
-   return false;
- }
- else return true;
-}
+// function isbusy(arg)
+// {
+//  if (arg != null)
+//  {
+//    alert("busy: "+arg); 
+//    return false;
+//  }
+//  else return true;
+// }
 
  
- 
-//  function kill(player, from, to) {
-//    var res;
-//    var x;
-//    var y;
-//     var line = []
-//  line[0] = [1,2,3,4];
-//  line[1] = [5,6,7,8];
-//  line[2] = [9,10,11,12];
-//  line[3] = [13,14,15,16];
-//  line[4] = [17,18,19,20];
-//  line[5] = [21,22,23,24];
-//  line[6] = [25,26,27,28];
-//  line[7] = [29,30,31,32];
-// 
-//  var opponent = position[to];
-// alert(to);
-// 
-// var pin = [0,0], pto = [0,0], pbetween = [0,0];
-// 
-// 
-// for(x = 0; x<= 7; x++)
-//   {
-//    for(y = 0; y <= 3; y++)
-//    {    
-//      if(from == line[x][y])
-//      {
-//       pin[0] = x;
-//       pin[1] = y;
-//      }
-//      else if (to == line[x][y]) 
-//      {
-//        pto[0] = x;
-//        pto[1] = y;
-//      }
-//    }
-//   }
-//   
-//   pbetween[0]
-//  
-//  if (player == "white" /*&& opponent == "black"*/ )
-//  {
-//   
-//     
-//   alert("legal kill");
-//   res = true;
-//  }
-//  else if(player == "black" /*&& opponent == "white"*/)
-//  {
-//   alert ("legal kill"); 
-//   res = true;
-//  }
-//  else 
-//  {
-//    alert("illegal moved!");
-//    res = false;
-//  }
-//  alert(pto[0]);
-//  alert(pto[1]);
-//  return res;
-//  }
