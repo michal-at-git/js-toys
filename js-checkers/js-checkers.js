35a6b98a2f8cf2a17268ae3dd7ca754b3ee17cac
michal-at-git <phantom-black@myopera.com>
2013-09-23 19:14:26 +0200

    bye bye old code

--- js-checkers/js-checkers.js
+++ js-checkers/js-checkers.js
@@ -35,9 +35,6 @@ function start() {
 var whitemv = true; // white first
 var from = {"x":false, "y":false}; 
 var to = {"x":false, "y":false};
-// var _in = 0;
-// var _out = 0;
-// var i = 0, j = 0;
 
 
 
@@ -73,21 +70,7 @@ function checkers(py, px)
 	from["x"] = from["y"] = to["x"] = to["y"] = false; 
 	alert("bad move!");
       }
-//     else {    alert("to"); //debug
-// 
-//       to["x"] = px;
-//       to["y"] = py;
-//       mv("white");
-//     }
-//    whitemv=false;
 
-//   }
-//   else if (from["x"] && from["y"]) {
-//     
-//    alert("legal move for black"); //debug
-//    whitemv=true;
-//   
-//   }
   
   highlight(px,py);
 }
@@ -97,7 +80,6 @@ function checkers(py, px)
 
 
 
-//will be renamed to move()! conflict with wariable name ;P
 function mv(pawn) {
  //lines 
  alert("mv()")
@@ -116,85 +98,7 @@ function mv(pawn) {
 from["x"] = from["y"] = to["x"] = to["y"] = false; 
  refresh();
 
- 
-/*
- from += 1;
- to += 1;
- var res = false;
- var line = []
- line[0] = [1,2,3,4];
- line[1] = [5,6,7,8];
- line[2] = [9,10,11,12];
- line[3] = [13,14,15,16];
- line[4] = [17,18,19,20];
- line[5] = [21,22,23,24];
- line[6] = [25,26,27,28];
- line[7] = [29,30,31,32];
-
-if (pawn == "white") 
- for (x = 0; x <= 7; x++)
- {
-   for (y = 0; y <= 3; y++) // it was  (y = 0; y <= 6; y++) - correct to 3 because table has only 4 elements
-   { 
-      if ((from == line[x][y]) && (to == line[x+1][y]))  
-      {
-	alert("legal move");
-	res = isbusy(position[to-1]);
-	x = 7;
-      }
-      else if ((x%2) != 0 && (from == line[x][y]) && (to == line[x+1][y+1])) // not finished & not good
-	{
-	  alert("legal move");
-	  res = isbusy(position[to-1]);
-	  x = 7;
-	}
-	else if ((x%2) == 0 && (from == line[x][y]) && (to == line[x+1][y-1])) // not finished & not good
-	{
-	  alert("legal move");
-	  res = isbusy(position[to-1]);
-	  x = 7;
-	}
-	
-   }
- }
- 
-else if (pawn == "black")
-{
-  for (x = 7; x >= 0; x--)
- {
-  
-   for (y = 3; y >= 0; y--) // it was  (; y <= 6; ) - correct to 3 because table has only 4 elements
-   { 
-      if ((from == line[x][y]) && (to == line[x-1][y]))  
-      {
-	alert("legal move");
-	res = isbusy(position[to-1]);
-	x = 0;
-      }
-      else if ((x%2) == 0 && (from == line[x][y]) && (to == line[x-1][y-1])) // UNSTABLE - NOT WORKING
-	{
-	  alert("legal move");
-	  res = isbusy(position[to-1]);
-	  x = 0;
-	}
-	else if ((x%2) != 0 && (from == line[x][y]) && (to == line[x-1][y+1])) // UNSTABLE - NOT WORKING
-	{
-	  alert("legal move");
-	  res = isbusy(position[to-1]);
-	  x = 0;
-	}
-	
-
-   }
- }
 }
-//if (!res) res = kill(position[from],from, to);
-
- 
- return res;
- 8*/
-}
-
 
 
 
@@ -227,32 +131,5 @@ function highlight(px, py)
 	borderColor = 'red';
     }
    
-//  if (move == 1)
-//  {
-//     var light = document.getElementById(board[py][px]);
-//     with (light.style) 
-//     {
-// 	borderColor = 'red';
-//     }
-//  }
-//     else{ 
-// 	var light = document.getElementById(board);
-// 	with (light.style) 
-// 	    {
-// 	      borderColor = '#000000';
-// 	    }
-// 	}
  }
-
- 
-// function isbusy(arg)
-// {
-//  if (arg != null)
-//  {
-//    alert("busy: "+arg); 
-//    return false;
-//  }
-//  else return true;
-// }
-
  
